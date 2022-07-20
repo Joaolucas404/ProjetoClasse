77b4ba478c8247be379c4f0549be47a111709ffc
joaolucas404 <joaolucasssa21@gmail.com>
2022-07-20 00:28:16 -0300

    Atividade Classe

---
+++ classe1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using classe1.Carro;
+using Carro.src;
+
+namespace Carro.src
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            carro tesla = new carro(4,"Tesla Model S\n", "Branco\n",2022);
+            Console.WriteLine(tesla.ToString());
+        }
+    }
+}
---
+++ classe1/Src/Carro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classe1.Carro
+{
+    public class carro
+    {
+        int QdtRodas { get; set; }
+        string Modelo { get; set; }
+        string Cor { get; set; }
+
+        internal string ToString(int v1, string v2, string v3, int v4)
+        {
+            throw new NotImplementedException();
+        }
+
+        int Ano { get; set; }
+
+        public carro()
+        {
+        }
+
+        public carro(int qdtRodas, string modelo, string cor, int ano)
+        {
+            QdtRodas = qdtRodas;
+            Modelo = modelo;
+            Cor = cor;
+            Ano = ano;
+        }
+
+        public carro(int modelo, int cor)
+        {
+            Modelo = "Modelo";
+            Cor = "cor";
+        }
+
+        public override string ToString()
+        {
+            return $"Modelo: {Modelo} - Cor: {Cor} - Ano: {Ano} - Quantidade de Rodas: {QdtRodas}";
+            
+        }
+    }
+}
